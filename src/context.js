256ff06e90d1a2f4a06648684913cd1e634f46fa
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-05-12 01:28:13 +0400

    Cocktails SetUp

--- src/context.js
+++ src/context.js
@@ -1,15 +1,18 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useCallback } from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const AppContext = React.createContext()
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  const context = useContext(AppContext);
 
-export { AppContext, AppProvider }
+  if (context === undefined) throw new Error("Used outside AppProvider");
+  return context;
+};
+
+export { AppContext, AppProvider };
